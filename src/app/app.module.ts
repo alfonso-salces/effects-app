7f9b9bd383188197a98fe7c77c24fbcb8ad75cd4
Alfonso <alfonsomtb98@gmail.com>
2020-01-06 14:26:20 +0100

    Inicializando repositorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    UsuariosModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/usuarios/usuario/usuario.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario.model';
+
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './usuario.component.html',
+  styles: []
+})
+export class UsuarioComponent implements OnInit {
+
+  constructor(public usuarioService: UsuarioService) { }
+
+  ngOnInit() {}
+
+}
